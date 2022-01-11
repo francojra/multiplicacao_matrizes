b1b7ea34ffd793aa8681da6067486f86bcbadaa7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-10 22:40:00 -0300

    Script

--- matriz_multiplicacao.R
+++ matriz_multiplicacao.R
@@ -5,7 +5,7 @@ M <- matrix(c(3,1,0, 0,2,1, 0,0,2), ncol = 3) # criando matriz 3 x 3
 s <- c(60,26,15) # Criando matriz 3 x 1
 invM <- solve(M) # Cálculo da matriz inversa M
 
-# Visualizando as matrizes criadas ---------------------------------------------------------------------------------------------------------
+# Visualização das matrizes criadas ---------------------------------------------------------------------------------------------------------
 
 M
 s
